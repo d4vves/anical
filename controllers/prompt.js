ffe8fefa74377f83b6a85b7875d774e5018f260d
d4vves <d4vves@gmail.com>
2020-07-06 17:26:16 -0500

    delete prompt route completed

--- controllers/prompt.js
+++ controllers/prompt.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const isLoggedIn = require('../middleware/isLoggedIn')
 const db = require('../models')
+const methodOverride = require('method-override')
+router.use(methodOverride('_method'))
 
 router.post('/', (req, res) => {
     db.user.findByPk(req.user.id)
@@ -12,6 +14,14 @@ router.post('/', (req, res) => {
     }).catch(err => {
         console.log(`🚦 ${err} 🚦`)
     })
+})
+
+router.delete('/:id', (req, res) => {
+    db.prompt.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(res.redirect('/profile'))
 })
 
 module.exports = router
