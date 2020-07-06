1541bd7602f14a5c2538b4f15bfd42f1f797ea90
d4vves <d4vves@gmail.com>
2020-07-06 13:54:07 -0500

    character post route started

--- controllers/character.js
+++ controllers/character.js
@@ -5,11 +5,21 @@ const axios = require('axios')
 const methodOverride = require('method-override')
 router.use(methodOverride('_method'))
 
+router.post('/', (req, res) => {
+    db.character.findOrCreate({
+        where: {
+            name: req.body.name
+        },
+        defaults: {
+            imageurl: req.body.imageurl
+        }
+    }).then(res.redirect('/profile'))
+})
+
 router.get('/search', (req, res) => {
     axios.get(`https://api.jikan.moe/v3/search/character?q=${req.query.name}`)
     .then(response => {
         let characters = response.data.results
-        console.log(`🤡 ${characters} 🤡`)
         res.render('character/characters', { characters })
     }).catch(err => {
         console.log(`🚦 ${err} 🚦`)
